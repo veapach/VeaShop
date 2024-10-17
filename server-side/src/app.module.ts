b1fe61af31ad1ad069e37aebd4722440f530bc17
veapach <egor_chapaev@bk.ru>
2024-10-17 16:56:11 +0300

    main setups finished

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import {ConfigModule} from '@nestjs/config'
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()
+  ],
 })
 export class AppModule {}
